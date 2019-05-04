622a33e01f2d6c0e008c34cc225592b39c0cd2cc
Moti666999 <48453182+Moti666999@users.noreply.github.com>
2019-05-04 12:32:34 +0300

    Update SeleniumT

--- SeleniumT.py
+++ SeleniumT.py
@@ -1,10 +1,19 @@
 # Import tools.
 from selenium import webdriver
+import time
 
 
-driver = webdriver.Chrome(executable_path="C:/Devops/chromedriver.exe")
-driver.implicitly_wait(10)
-driver.get("http://192.168.99.100:5000")
-text = driver.find_element_by_xpath("//html/body")
-print(text.text)
-driver.quit()
+def test():
+    # Get Chrome Driver.
+    driver = webdriver.Chrome(executable_path="C:/Devops/chromedriver.exe")
+    driver.implicitly_wait(10)
+    # Open the Window.
+    driver.get("http://192.168.99.100:5000")
+    text = driver.find_element_by_xpath("//html/body")
+    print(text.text)
+    time.sleep(3)
+    driver.quit()
+
+
+if __name__ == '__main__':
+    test()
