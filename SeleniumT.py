09603d33334f6a9c4d5829d7485ba4f4ee88de4b
Moti666999 <48453182+Moti666999@users.noreply.github.com>
2019-05-04 15:56:05 +0300

    Update SeleniumT.py

--- SeleniumT.py
+++ SeleniumT.py
@@ -6,12 +6,17 @@ import time
 def test():
     # Get Chrome Driver.
     driver = webdriver.Chrome(executable_path="C:/Devops/chromedriver.exe")
+    # Time to get the tools.
     driver.implicitly_wait(10)
-    # Open the Window.
+    # Open the Site.
     driver.get("http://192.168.99.100:5000")
+    # Find the element.
     text = driver.find_element_by_xpath("//html/body")
-    print(text.text)
+    new_text = str(text.text).replace(" World", " ")
+    print(new_text)
+    # Print the txt.
     time.sleep(3)
+    # Close the Site.
     driver.quit()
 
 
